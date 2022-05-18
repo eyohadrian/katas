167636bbfc60731ec6bdcc5d8ee43216a5c1e743
Adrian <aasensiomtd@gmail.com>
2022-05-18 09:55:33 +0200

    Completed bs

--- binary_search/bs.py
+++ binary_search/bs.py
@@ -1,27 +1,19 @@
 import math
 
 def search(nums, target):
-    l=0
-    r=len(nums) - 1
-    index_target = -1
+    l,r= 0, len(nums) - 1
 
-    while (l != r and nums[index_target] != target):
-        if (nums[l] == target):
-            index_target = l
+    while l <= r:
+        m = r + l // 2
+        if nums[m] < target:
+            l = m + 1
+        elif nums[m] > target:
+            r = m - 1
+        else:
+            return m
+    return -1
 
-        if (nums[r] == target):
-            index_target = r
-
-        if (nums[index_target] != target):
-            m = math.floor((r + l) / 2)
-            if (nums[m] == target):
-                index_target = m
-            elif (nums[m] > target):
-                r = m
-            elif (nums[m] < target):
-                l = m
-
-    return index_target
-
-
-assert search([-1, 2, 3], 9) == 3
+assert search([-1,0,3,5,9,12], 9) == 4
+assert search([-1, 2, 3], 9) == -1
+assert search([-1, 2, 3, 9], 9) == 3
+assert search([-1,0,3,5,9,12], 2) == -1
