81cea53b7fb241f2da0d279a5aafabc7ab992875
Adrian <adrian.asensio.bdn@gmail.com>
2022-06-02 22:02:01 +0200

    LCA tree

---
+++ trees/lca.py
@@ -0,0 +1,10 @@
+def lowestCommonAncestor(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    curr = root
+
+    while curr:
+        if p.val > curr.val and q.val > curr.val:
+            curr = curr.right
+        elif q.val < curr.val and p.val < curr.val:
+            curr = curr.left
+        else:
+            return curr
